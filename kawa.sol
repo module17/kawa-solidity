c26a258bb36d16bd0c3472dbca402b9609ab16aa
Darek Kowalski <module17@gmail.com>
2017-11-29 09:17:17 -0500

    Symbol, name, features

--- kawa.sol
+++ kawa.sol
@@ -1,12 +1,18 @@
 pragma solidity ^0.4.11;
 
 contract Kawa {
-    string greet = "Kawa is great.";
+    string public constant symbol = "KAWA";
+    string public constant name = "Kawa Coin";
+    uint8 public constant decimals = 3;
+    string public greet = "Kawa is great.";
+    uint public totalKawa = 17000.000;
     address creator;
     mapping (address => uint) private balances;
+    mapping (address => mapping(address => uint)) allowances;
     
     function Kawa() public {
         creator = msg.sender;
+        balances[msg.sender] = totalKawa;
     }
     
     event Log(string msg);
@@ -43,17 +49,42 @@ contract Kawa {
     function brewKawa(address receiver, uint amount) public isCreator {
         require(receiver != address(0) && amount > 0);
         balances[receiver] += amount;
+        totalKawa += amount;
     }
     
     function sendKawa(address receiver, uint amount) public {
         require(receiver != address(0) && amount > 0 && balances[msg.sender] > amount);
         balances[msg.sender] -= amount;
         balances[receiver] += amount;
-        LogTrx('Coins sent.', msg.sender, receiver, amount);
+        LogTrx('Kawa sent.', msg.sender, receiver, amount);
     }
-    
+
     function checkKawaBalance(address wallet) public constant returns(uint) {
         require(wallet != address(0));
         return balances[wallet];
+    }
+
+    function sendKawaFrom(address from, address receiver, uint amount) public {
+        require(
+            from != address(0) &&
+            receiver != address(0) &&
+            amount > 0 &&
+            balances[from] >= amount &&
+            allowances[from][msg.sender] >= amount
+            );
+        balances[from] -= amount;
+        balances[receiver] += amount;
+        allowances[from][msg.sender] -= amount;
+        LogTrx('Kawa transferred.', from, receiver, amount);
+    }
+
+    function allowKawaTransfer(address spender, uint amount) public {
+        require(spender != address(0) && amount > 0);
+        allowances[msg.sender][spender] = amount;
+        LogTrx('Allowed Kawa transfer.', msg.sender, spender, amount);
+    }
+
+    function checkKawaAllowance(address owner, address spender) public constant returns (uint allowance) {
+        return allowances[owner][spender];
     }
 }
